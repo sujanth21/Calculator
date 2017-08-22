682dda13bc493cfa2e93f4f9b912eb2c69cdd97f
Sujanth <sujanth1987@gmail.com>
2017-08-22 20:37:01 +1000

    Actions and outlets added

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    var isFirstDigit = true
+    var operation1: Double = 0
+    var operation2 = "="
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: label.text!)!.doubleValue
+        }
+        set {
+            label.text = String(format: "%2.0f", newValue)
+            isFirstDigit = true
+            operation2 = "="
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,26 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func digitBtnPressed(_ sender: AnyObject) {
+        
+        let digit = sender.currentTitle!
+        
+        label.text = isFirstDigit ? digit : label.text! + digit!
+        isFirstDigit = false
+    }
+    
+    @IBAction func cancelBtnPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func calculateBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func operationBtnPressed(_ sender: Any) {
+    }
+    
+    
 }
 
